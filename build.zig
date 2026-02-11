96ecb2c17130b833bb7ef2e32e3c533d0a839334
Cal Seagram <143629409+seagram@users.noreply.github.com>
2026-02-11 15:00:13 -0500

    feat: link sqlite3 library

--- build.zig
+++ build.zig
@@ -4,31 +4,34 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardOptimizeOption(.{});
 
-    const module = b.addModule("main", .{
-        .target = target,
-        .optimize = mode,
-        .link_libc = true,
-    });
-
-    module.addCSourceFile(.{
-        .file = b.path("src/main.c"),
-        .flags = &.{"-Wall", "-Wextra"},
-    });
-
     const exe = b.addExecutable(.{
         .name = "imessage-query",
-        .root_module = module,
+        .root_module = b.createModule(.{
+            .target = target,
+            .optimize = mode,
+        }),
     });
+
+    exe.addCSourceFiles(.{
+        .files = &[_][]const u8{
+            "src/main.c",
+            "src/utils.c",
+        },
+        .flags = &[_][]const u8{ "-std=c11", "-Wall", "-Wextra" },
+    });
+
+    exe.addIncludePath(b.path("include"));
+
+    exe.linkLibC();
+    exe.linkSystemLibrary("sqlite3");
 
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
-
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
-    const run_step = b.step("run", "the app");
+    const run_step = b.step("run", "");
     run_step.dependOn(&run_cmd.step);
 }
